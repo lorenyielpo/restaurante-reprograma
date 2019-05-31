c13239cd12276deb2432af4b70512a4a2a4845a3
Loreny Ielpo <lorenyielpo@gmail.com>
2019-05-30 22:26:33 -0300

    Exclusão da const comidas

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// const comidas = [
-//   {
-//     nome: "Batata frita",
-//     descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-//     imagem: "img/Batata-frita.jpg"
-//   },
-//   {
-//     nome: "Macarronada",
-//     descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-//     imagem: "img/macarronada.jpg"
-//   },
-//   {
-//     nome: "Falafel",
-//     descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-//     imagem: "img/falafel.jpg"
-//   },
-//   {
-//     nome: "Creme de abóbora",
-//     descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-//     imagem: "img/creme-de-abobora.jpg"
-//   },
-
-// ]
-
 const container = document.querySelector('#items-cardapio')
 fetch('http://localhost:3000/comidas')
   .then((response) => {
