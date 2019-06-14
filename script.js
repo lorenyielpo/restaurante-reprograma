54ec9c948ecd71ce2ef4ebc2ddd0ea96dd618a1a
Loreny Ielpo <lorenyielpo@gmail.com>
2019-06-13 22:06:25 -0300

    Delete no front

--- script.js
+++ script.js
@@ -19,10 +19,52 @@ fetch('http://localhost:3000/comidas')
                 ${prato.descricao}
               </div>`
       container.appendChild(mediaItem);
-    }
-    )
-  }
-  )
+
+      const btnRemove = document.createElement('button')
+      btnRemove.innerHTML = "X"
+      btnRemove.setAttribute('type', 'button')
+      btnRemove.setAttribute('data-id', `${prato._id}`)
+      btnRemove.setAttribute('class', 'btn btn-dark')
+      mediaItem.appendChild(btnRemove)
+
+      btnRemove.addEventListener('click', () => {
+
+        fetch(`http://localhost:3000/comidas/${prato._id}`, {
+          method: 'DELETE',
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        .then(()=>{
+          container.removeChild(mediaItem)
+        })
+        .catch((erro)=>{
+          console.log(erro)
+        })
+      })
+    })
+  })
   .catch((erro) => {
     console.log(erro)
   })
+
+const btn = document.querySelector('#criar_comida_button')
+btn.addEventListener('click', () => {
+
+  const nome = document.querySelector('#nome_input').value
+  const descricao = document.querySelector('#descricao_input').value
+  const imagem = document.querySelector('#imagem_input').value
+  const comida = {
+    nome,
+    descricao,
+    imagem
+  }
+
+  fetch('http://localhost:3000/comidas', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(comida)
+  })
+})
